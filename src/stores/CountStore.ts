fe9af950d05bab6df715c1418ae10010e1ff89d1
kobakakzu0429 <kobakazu10@gmail.com>
2018-06-23 10:09:10 +0900

    fix

--- src/stores/CountStore.ts
+++ src/stores/CountStore.ts
@@ -1,27 +1,27 @@
 import { action, computed, observable } from "mobx";
 
-export type CountStoreType = {
+export interface CountStoreType {
   num: number;
   getDoubleCount: () => void;
   onIncrement: () => void;
   onDecrement: () => void;
-};
+}
 
 export default class CountStore {
-  @observable num = 0;
+  @observable private num = 0;
 
-  @computed
+  @computed // observableが変わると同時に変わる
   get getDoubleCount() {
     return this.num * 2;
   }
 
-  @action.bound
-  onIncrement() {
+  @action.bound // vueでいうmutatinos stateを変えれる
+  public onIncrement() {
     this.num = this.num + 1;
   }
 
   @action.bound
-  onDecrement() {
+  public onDecrement() {
     this.num = this.num - 1;
   }
 }
